9ee5b579c59f49e52c2f942d09f629bd0ae98eea
we5046 <we5046@naver.com>
2026-04-09 17:34:28 +0900

    fixed: 사용자 이름 데이터 undefined 문제 해결

--- TeamProject/src/layouts/AuthLayout.vue
+++ TeamProject/src/layouts/AuthLayout.vue
@@ -6,12 +6,4 @@
 
 <script setup></script>
 
-<style scoped>
-.auth-layout {
-  min-height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: #f5f5f5;
-}
-</style>
+<style scoped></style>
